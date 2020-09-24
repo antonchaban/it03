565c3129255e61f20e83a16ac79a7b7c3502e357
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2020-09-25 00:41:28 +0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,8 @@ public class helloworld
 {
     public static void main(String[] args)
     {
+        String s = "Line2";
         System.out.println("Hello, World!");
+        System.out.print(s);
     }
 }
